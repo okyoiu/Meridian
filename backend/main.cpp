289d49c3fd824899d4bd88eabddc49c259e1335e
okyoiu <cesar.sanc219@gmail.com>
2026-05-31 01:35:30 -0500

    feat: implemented skeleton for parser.hpp as well as main.cpp file for program to enter

--- backend/main.cpp
+++ backend/main.cpp
@@ -1,1 +1,25 @@
 // PURPOSE: main entry point for the program
+#include <iostream>
+#include <string>
+#include "parser.hpp"
+
+// gateway entry for program
+int main() 
+{
+    std::string data_path = "../data/austin.osm.pbf";
+
+    std::cout << "Starting Meridian Routing Engine" << std::endl;
+    std::cout << "Loading map data from: " << data_path << std::endl;
+
+    try {
+        Graph road_network = parse_map_data(data_path);
+
+        std::cout << "\n--- MAP LOADING COMPLETE ---" << std::endl;
+        std::cout << "Total Nodes Loaded: " << road_network.nodes.size() << std::endl;
+        std::cout << "Total Adjacency Lists: " << road_network.adjList.size() << std::endl;
+    } catch (const std::exception& e) {
+        std::cerr << "Error Loading map data: " << e.what() << std::endl;
+        return 1;
+    }
+    return 0;
+}
--- backend/parser.hpp
+++ backend/parser.hpp
@@ -1,1 +1,34 @@
 // Header file for data structures
+#ifndef PARSER_HPP
+#define PARSER_HPP
+
+#include <vector>
+#include <cstdint> // note: provides fixed-withd int
+#include <string>
+
+// A single point that is on the Earth
+struct Node 
+{
+    int64_t osm_id; // massible global ID from OpenStreetMap
+    double lat; // latitude
+    double lon; // longitude
+    uint32_t index; // tight internal index for our array
+};
+
+// A road connection between the nodes
+struct Edge 
+{
+    uint32_t to; // internal index of the destination node (route)
+    float weight; // distance or travel time (graph theory def.)
+};
+
+struct Graph
+{
+    std::vector<Node> nodes;
+    std::vector<std::vector<Edge>> adjList; // adjacent List holds outdegree edges for node `i`
+};
+
+// Function declaration to parse the file for information
+Graph parse_map_data(const std::string& file_path);
+
+#endif // END_OF PARSER_HPP
