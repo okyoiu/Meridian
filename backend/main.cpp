6a835022cc5f9721f865a6630de443636b1dad02
okyoiu <cesar.sanc219@gmail.com>
2026-06-11 19:17:34 -0500

    fix: added implementation for implementing the time, still needs to be connected with frontend, but backend server holds values

--- backend/main.cpp
+++ backend/main.cpp
@@ -4,6 +4,7 @@
 #include <unordered_map>
 #include "parser.hpp"
 #include "router.hpp"
+#include <chrono> // for timing
 
 #include <crow.h>
 #include <nlohmann/json.hpp>
@@ -24,12 +25,18 @@ int main()
     // declaring the graph outside so that it runs for the whole program
     Graph road_network;
 
+    auto load_start = high_resolution_clock::now();
+
     try {
         road_network = parse_map_data(data_path);
+
+        auto load_end = high_resolution_clock::now();
+        auto load_duration = duration_cast<miliseconds>(load_end - load_start).count();
 
         std::cout << "\n--- MAP LOADING COMPLETE ---" << std::endl;
         std::cout << "Total Nodes Loaded: " << road_network.nodes.size() << std::endl;
         std::cout << "Total Adjacency Lists: " << road_network.adjList.size() << std::endl;
+        std::cout << "Load Time: " << load_duration << " ms" << std::end;
     } catch (const std::exception& e) {
         std::cerr << "Error Loading map data: " << e.what() << std::endl;
         return 1;
@@ -41,6 +48,16 @@ int main()
     // Boot up the router with our loaded map
     Router router(road_network);
     crow::SimpleApp app;
+
+    CROW_ROUTE(app, "/route").methods("OPTIONS"_method)
+    ([](const crow::request& req){
+        crow::response res(204);
+        res.set_header("Access-Control-Allow-Origin", "*");
+        res.set_header("Access-Control-Allow-Methods", "GET, OPTIONS");
+        res.set_header("Access-Control-Allow-Headers", "Content-Type");
+        return res;
+    });
+
 
     // This is our API Endpoint. It listens for dynamic physical coordinates!
     CROW_ROUTE(app, "/route")
@@ -60,6 +77,9 @@ int main()
         double start_lon = std::stod(start_lon_param);
         double end_lat = std::stod(end_lat_param);
         double end_lon = std::stod(end_lon_param);
+        
+        // starting the timer to the entire route computation
+        auto route_start = high_resolution_clock::now();
 
         // 2. Find the closest actual roads to the mouse clicks!
         uint32_t start_index = router.find_nearest_node(start_lat, start_lon);
@@ -67,13 +87,17 @@ int main()
 
         // 3. Execute Dijkstra!
         RouteResult route = router.find_shortest_path(start_index, end_index);
-
+        // fix: ending the timing
+        auto route_end = high_resolution_clock::now();
+        auto duration_ms = duration_cast<microseconds>(route_end - route_start).count() / 1000.0;
         if (!route.found) return crow::response(404, "No path exists.");
 
         // 4. Serialize to JSON
         json response_json;
         response_json["distance_meters"] = route.total_distance_meters;
-        
+        response_json["duration_ms"] = round(duration_ms * 100) / 100; // NEW: 2 decimal places
+        response_json["nodes_visited"] = route.nodes_visited; 
+
         json geometry = json::array();
         for (uint32_t node_idx : route.path_indices) {
             const Node& n = road_network.nodes[node_idx];
